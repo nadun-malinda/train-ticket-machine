50ad02dbc5da126ec5543bd9f1a5b69d61fb4337
Nadun Malinda <bknandun@gmail.com>
2025-03-21 09:14:34 +0100

    update: keyboard input component to receive station list and render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { KeyboardInput } from "@/components/keyboard-input";
+import { fetchStations } from "../lib/api";
 
-export default function Home() {
+export default async function Home() {
+  const stations = await fetchStations();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-3xl font-semibold">Train Ticket Machine</h1>
 
-        <KeyboardInput />
+        <KeyboardInput stations={stations} />
       </main>
     </div>
   );
--- src/components/keyboard-input.tsx
+++ src/components/keyboard-input.tsx
@@ -2,9 +2,17 @@
 
 import { useState } from "react";
 import { Keyboard } from "./keyboard/keyboard";
+import { Station } from "@/types/station";
+import { filterStations } from "@/utils/station";
+import { StationList } from "./station-list";
 
-export function KeyboardInput() {
+interface KeyboardInputProps {
+  stations: Station[];
+}
+
+export function KeyboardInput({ stations }: KeyboardInputProps) {
   const [text, setText] = useState("");
+  const filteredStations = filterStations(text, stations);
 
   const handleClick = (key: string) => {
     setText((prev) => {
@@ -15,9 +23,22 @@ export function KeyboardInput() {
   };
 
   return (
-    <div>
-      <h2>User input: {text}</h2>
-      <Keyboard onClick={handleClick} />
+    <div className="grid grid-cols-2 gap-4">
+      <div>
+        <p className="text-xl font-medium mb-4">User input: {text}</p>
+        <Keyboard onClick={handleClick} />
+      </div>
+      <div>
+        <div>
+          <p className="text-xl font-medium mb-4">
+            Stations{" "}
+            <span className="text-sm ml-2">
+              ({filteredStations.length} found)
+            </span>
+          </p>
+          <StationList stations={filteredStations} />
+        </div>
+      </div>
     </div>
   );
 }
