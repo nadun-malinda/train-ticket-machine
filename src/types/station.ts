01cb0c3c8ba18d9459db2eecd7602bfd637a78a7
Nadun Malinda <bknandun@gmail.com>
2025-03-21 09:11:39 +0100

    add: station type definition

---
+++ src/types/station.ts
@@ -0,0 +1,14 @@
+/**
+ * Represents a train station.
+ */
+export interface Station {
+  /**
+   * The unique code of the station.
+   */
+  stationCode: string;
+
+  /**
+   * The name of the station.
+   */
+  stationName: string;
+}
